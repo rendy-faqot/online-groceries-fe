4bba553d12b48a7163f9695185e193a1d7409873
rendyfaqot <rendy.faqot@live.com>
2021-06-01 20:47:19 +0700

    initial commit for landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  products = [];
+  details = {
+    name: '',
+    description: '',
+    imageUrl: '',
+    qty: 0
+  };
+	constructor(private apiService: ApiService) { }
+
   title = 'online-groceries-fe';
+  display = "none";
+
+  ngOnInit() {
+		this.apiService.sendGetRequest().subscribe((data: Product)=>{  
+			console.log(data);  
+			this.products = data.items;  
+		})  
+	}
+
+  openDetail(id){
+    console.log(id);
+    this.display = 'block';
+    this.details = this.products.filter(x => x.id === id)[0];
+    console.log(this.details);
+  }
+
+  closeModal(){
+    this.display = 'none';
+  }
+}
+
+export interface Product {
+  readonly items: any[];
 }
